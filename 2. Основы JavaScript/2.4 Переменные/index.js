156e276c20bd80073b20e20e2fa6cb5db99201a6
naqbas <120418276+naqbas@users.noreply.github.com>
2023-03-02 00:07:22 +0600

    Add files via upload

---
+++ 2. Основы JavaScript/2.4 Переменные/index.js
@@ -0,0 +1,13 @@
+//Задача 1
+let admin, name;
+name = "Джон";
+admin = name;
+alert(admin);
+
+//Задача 2
+let planetName = "Земля";
+let currentUserName = "Тима";
+
+//Задача 3
+const BIRTHDAY='18.04.1982';
+const age=someCode(BIRTHDAY);
